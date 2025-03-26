d2c188a27e70af629213063021858d4e22bf8d0f
Derek W. <derekoob@hotmail.com>
2025-03-26 20:17:33 +0000

    Added boxes to see the space needed

---
+++ app/components/blackbox.tsx
@@ -0,0 +1,19 @@
+
+
+
+function Blackbox() {
+    return (
+        <div className="flex justify-center">
+            <div className="text-white bg-black w-[1420px]  h-[800px] h-screen">
+                <div className="flex justify-center">
+                    <h1>H1 BlackBOX</h1>
+                </div>
+                <div className="p-8">
+                    <button className="bg-white text-black border-2 border-red-950 rounded-full p-5">Add note</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Blackbox
---
+++ app/components/sidebox.tsx
@@ -0,0 +1,20 @@
+function Sidebox() {
+    return (
+        <div className="fixed top-0 left-0 h-full w-64 bg-gray-200">
+            <div className="f ">
+                <h1 className="text-black text-center mt-4">H1 Sidebox</h1>
+            </div>
+            <div className="text-black pt-20 space-y-12 p-8">
+                <h2>Option 1</h2>
+                <h2>Option 2</h2>
+                <h2>Option 3</h2>
+                <h2>Option 4</h2>
+                <h2>Option 5</h2>
+                <h2>Option 6</h2>
+            </div>
+        </div>
+
+    );
+}
+
+export default Sidebox;
